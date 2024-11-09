99b50a648cf64fff213169ef5ad0bc3edddf078d
Jose Manuel <jmmb.pyd@gmail.com>
2024-11-09 17:15:55 +0100

    cambios añadidos a main despues de añadir a cabecera

--- Programa/ejemplo.py
+++ Programa/ejemplo.py
@@ -1,3 +1,5 @@
 # Programa ejemplo para probar git
 # modificación despues de hacer commit
 # Modificación despues de crear segunda rama
+# Añado dos lineas tras hacer commit en cabecera
+# esta es la segunda linea
--- Programa/segundarama.py
+++ Programa/segundarama.py
@@ -1,1 +1,2 @@
 # Con este documento creamos la segunda rama
+# este cambio se ha hecho desde main
